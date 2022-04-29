1b0f5dec62a1dcade050bfce6dbf88ea51559da6
Zesty <zethsatentes@yahoo.com>
2022-04-28 23:27:16 -0700

    get/post server.js

--- server.js
+++ server.js
@@ -12,16 +12,25 @@ app.use(express.json());
 
 
 app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
 
 app.get("*", (req, res) => {
-    let newNote =req.body;
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteLength = (noteList.noteLength).toString();
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-    newNote.id = noteLength;
+
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let notelength = (noteList.length).toString();
+
+    newNote.id = notelength;
 
     noteList.push(newNote);
 
@@ -29,10 +38,10 @@ app.get("*", (req, res) => {
     res.json(noteList);
 })
 
-
 app.delete("/api/notes/:id", (req, res) => {
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let noteId = (req.params.id).toString();
+
 
     noteList = noteList.filter(selected =>{
         return selected.id != noteId;
@@ -41,5 +50,6 @@ app.delete("/api/notes/:id", (req, res) => {
     fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
     res.json(noteList);
 });
+
 
 app.listen(PORT, () => console.log("Server listening on port " + PORT));
